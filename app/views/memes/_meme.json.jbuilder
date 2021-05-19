84d581bad01bc2631c8603891d66027a2ab2af63
Tayyat1390 <tayyat1390@gmail.com>
2021-05-19 22:43:40 +0000

    Changing Project Direction

--- app/views/memes/_meme.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! meme, :id, :created_at, :updated_at
-json.url meme_url(meme, format: :json)
--- db/migrate/20210519013756_create_memes.rb
+++
@@ -1,8 +0,0 @@
-class CreateMemes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :memes do |t|
-
-      t.timestamps
-    end
-  end
-end
